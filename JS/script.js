eb1a9a10ba8a5f4b4861526ca31e9a2814fd9a8a
Dayna-G <143474636+Dayna-G@users.noreply.github.com>
2024-02-25 17:25:08 -0800

    created the closing button

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,27 @@
 function newItem(){
-   //Adding new item to the list 
-    let li = $('<li></li>');
-    let inputValue = $('#input').val();
-    li.append(inputValue);
+     //Adding new item to the list 
+        let li = $('<li></li>');
+        let inputValue = $('#input').val();
+        li.append(inputValue);
     
     
     
-    if ($('#input').val() === "") {
-        alert ( "You must write something");
-    }else{
-      let list = $('#list'); 
-      list.append(li);  
-    }
+        if ($('#input').val() === "") {
+            alert ( "You must write something");
+        }else{
+            let list = $('#list'); 
+            list.append(li);  
+        }
 
     // Crossing out item in list 
         li.on("dblclick", function(){
             li.toggleClass("strike");
         });
-  
+    
+    // Adding the "X" button
+        let crossOutButton = $("<crossOutButton></crossOutButton>");
+        $(crossOutButton).append("X");
+        li.append(crossOutButton);
+
+    
 };
